e4473abe6f1e4b19fb8f60aba7f13e3e1818aa01
musyokajames <124431931+musyokajames@users.noreply.github.com>
2024-08-26 22:07:39 +0300

    Add files via upload

---
+++ go_projects/Todo_API_with_authentication/go.mod
@@ -0,0 +1,16 @@
+module APIwithAuthentication
+
+go 1.23.0
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/gorilla/mux v1.8.1
+	golang.org/x/crypto v0.26.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
+)
---
+++ go_projects/Todo_API_with_authentication/main.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func userRegistration(w http.ResponseWriter, r *http.Request) {
+	// connecting to my database
+	datasourcename := "Musyoka:mysqlpassword@tcp(127.0.0.1:3306)/users_db"
+	db, err := sql.Open("mysql", datasourcename)
+	if err != nil {
+		http.Error(w, "Error connecting to database", http.StatusInternalServerError)
+	}
+	defer db.Close()
+
+	// link the html form with my code
+	if r.Method == http.MethodGet {
+		tmpl := template.Must(template.ParseFiles("templates/registration.html"))
+		tmpl.Execute(w, nil)
+
+	} else if r.Method == http.MethodPost {
+
+		r.ParseForm()
+		name := r.FormValue("name")
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// generate a hash password
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Println("Error generating hash password:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+
+		}
+
+		// now insert these values to the database
+		query := "INSERT INTO users(name ,username, password_hash) VALUES (?, ?, ?)"
+		_, err = db.Exec(query, name, username, hashedPassword)
+		if err != nil {
+			log.Println("Error writing to SQL database", err)
+			http.Error(w, "Error writing to SQL database", http.StatusInternalServerError)
+			return
+		}
+		fmt.Println("Data inserted successfully")
+
+	}
+
+}
+
+var jwtSecret = []byte("mySecretKey")
+
+func userLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		loginTmpl := template.Must(template.ParseFiles("templates/login.html"))
+		loginTmpl.Execute(w, nil)
+
+	} else if r.Method == http.MethodPost {
+		r.ParseForm()
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// Connect to the database
+		datasourcename := "Musyoka:mysqlpassword@tcp(127.0.0.1:3306)/users_db"
+		db, err := sql.Open("mysql", datasourcename)
+		if err != nil {
+			http.Error(w, "Error connecting to database", http.StatusInternalServerError)
+			return
+		}
+		defer db.Close()
+
+		// Query database to fetch stored password
+		var storedHashedPassword string
+		query := "SELECT password_hash FROM users WHERE username = ?"
+		err = db.QueryRow(query, username).Scan(&storedHashedPassword)
+		if err == sql.ErrNoRows {
+			fmt.Println("User not found")
+			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
+			return
+		} else if err != nil {
+			fmt.Println("Error querying database:", err)
+			http.Error(w, "Error querying database", http.StatusInternalServerError)
+			return
+		}
+
+		//Compare hash and password
+		err = bcrypt.CompareHashAndPassword([]byte(storedHashedPassword), []byte(password))
+		if err != nil {
+			fmt.Println("Password incorrect")
+			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
+			return
+		}
+
+		fmt.Println("Login Successfull")
+
+		// If Login is successful create a JWT token
+		claims := jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(), //token expires in 24 hours
+		}
+
+		//Generating the JWT token.
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		//Signing the JWT
+		tokenString, err := token.SignedString(jwtSecret)
+		if err != nil {
+			http.Error(w, "Error generating token", http.StatusInternalServerError)
+			return
+		}
+
+		//Sending the token to the client
+		w.Header().Set("Authorization", "Bearer "+tokenString)
+		fmt.Fprintf(w, "Login successful, token: %s", tokenString)
+
+	}
+}
+
+// Middleware to Protect Routes
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		//Parse the token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		//Continue to next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Explanation string `json:"explanation"`
+}
+
+var tasks []Task
+
+func viewTasks(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/tasks.html"))
+	tmpl.Execute(w, tasks)
+}
+
+func viewTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	idStr := params["id"]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+
+	for _, item := range tasks {
+		if item.ID == id {
+			tmpl := template.Must(template.ParseFiles("templates/task.html"))
+			tmpl.Execute(w, item)
+			return
+		}
+	}
+	http.Error(w, "Page not found", http.StatusNotFound)
+}
+
+func addTasks(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+
+		newTask := Task{
+			ID:          len(tasks) + 1,
+			Title:       r.FormValue("title"),
+			Explanation: r.FormValue("explanation"),
+		}
+
+		tasks = append(tasks, newTask)
+
+		http.Redirect(w, r, "/view", http.StatusSeeOther)
+	} else {
+		http.ServeFile(w, r, "templates/new_task.html")
+		return
+	}
+}
+
+func deleteTasks(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	idStr := params["id"]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+
+	for index, item := range tasks {
+		if item.ID == id {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			break
+		}
+
+	}
+	http.Redirect(w, r, "/view", http.StatusSeeOther)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/register", userRegistration).Methods("GET", "POST")
+	r.HandleFunc("/login", userLogin).Methods("GET", "POST")
+
+	//Protected routes (Require authentication)
+	r.Handle("/view", authMiddleware(http.HandlerFunc(viewTasks))).Methods("GET")
+	r.Handle("/view/{id}", authMiddleware(http.HandlerFunc(viewTask))).Methods("GET")
+	r.Handle("/add", authMiddleware(http.HandlerFunc(addTasks))).Methods("POST", "GET")
+	r.Handle("/delete/{id}", authMiddleware(http.HandlerFunc(deleteTasks))).Methods("POST")
+
+	fmt.Println("Starting port on server 8080....")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
